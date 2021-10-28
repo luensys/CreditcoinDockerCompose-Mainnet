e637774c83f9f82944c8ec8212d9f0acbf4a0296
Jaesun Jeong <jaesun.jeong@gmail.com>
2021-10-28 18:50:11 +0900

    get tip without client container

--- monitor.sh
+++ monitor.sh
@@ -14,22 +14,22 @@ function run_creditcoin_client {
 }
 
 function get_block_tip {
-  TipBlkNum=$( docker exec -it creditcoin-client ./ccclient tip | awk '/^[0-9]/{print int($0)}')
-  return $TipBlkNum
+  curl -m 60 http://$REST_API_ENDPOINT/blocks?limit=1 2>/dev/null | grep -o '"block_num": "[^"]*' | cut -d'"' -f4
+  return $?
 }
-
 
 
 [ -z $REST_API_ENDPOINT ]  &&  REST_API_ENDPOINT=localhost:8008
 
-run_creditcoin_client
+#run_creditcoin_client
 
 #docker-compose -f /home/happymine/CreditCoin/Client/docker-compose.yaml up -d
 
 while true
 do
 	timestamp
-  	TipBlkNum=$(docker exec -it creditcoin-client ./ccclient tip | awk '/^[0-9]/{print int($0)}')
+  	#TipBlkNum=$(docker exec -it creditcoin-client ./ccclient tip | awk '/^[0-9]/{print int($0)}')
+	TipBlkNum=$(get_block_tip)
 	echo -n "  $TipBlkNum "
 	echo `curl -m 60 http://$REST_API_ENDPOINT/peers 2>/dev/null | grep tcp:// | cut -d \" -f2 | sed 's/^.*\///'`
 	sleep 60
