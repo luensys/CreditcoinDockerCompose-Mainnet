381f6155dfeae48b661b463ada01f0abc0b3480f
Kiseop Lee <kiseop.lee@gmail.com>
2021-11-24 21:39:29 +0900

    Update docker-compose_change.sh

--- scripts/docker-compose_change.sh
+++ scripts/docker-compose_change.sh
@@ -2,5 +2,5 @@
 
 source $CREDITCOIN_HOME_UNDER/.env
 
-sed -i "s/- 8800/- $PORT/g" $CREDITCOIN_HOME/docker-compose.yaml
-sed -i "s/tcp://[insert.your.ip]:8800/tcp://$IP_ADDR:$PORT/g" $CREDITCOIN_HOME/docker-compose.yaml
+sed -i "s|- 8800|- $PORT|g" $CREDITCOIN_HOME/docker-compose.yaml
+sed -i "s|tcp:\/\/\[insert.your.ip\]:8800|tcp:\/\/$IP_ADDR:$PORT|g" $CREDITCOIN_HOME/docker-compose.yaml
