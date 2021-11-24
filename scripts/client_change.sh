fdfffde68b58951c0aecfa0e51a2da14116ac29d
Kiseop Lee <kiseop.lee@gmail.com>
2021-11-24 21:38:47 +0900

    Update client_change.sh

--- scripts/client_change.sh
+++ scripts/client_change.sh
@@ -2,6 +2,6 @@
 
 source $CREDITCOIN_HOME_UNDER/.env
 
-sed -i "s/<256_bit_key_secp256k1_ECDSA>/$PRIV_KEY/g" $CREDITCOIN_HOME_UNDER/clientConfig.json
-sed -i "s/<ethereum_node_url>/$INFURA/g" $CREDITCOIN_HOME_UNDER/clientConfig.json
-sed -i "s/<ethereum_private_key_no_0x>/$SECRET_KEY/g" $CREDITCOIN_HOME_UNDER/clientConfig.json
+sed -i "s|<256_bit_key_secp256k1_ECDSA>|$PRIV_KEY|g" $CREDITCOIN_HOME_UNDER/Client/clientConfig.json
+sed -i "s|<ethereum_node_url>|$INFURA|g" $CREDITCOIN_HOME_UNDER/Client/clientConfig.json
+sed -i "s|<ethereum_private_key_no_0x>|$SECRET_KEY|g" $CREDITCOIN_HOME_UNDER/Client/clientConfig.json
