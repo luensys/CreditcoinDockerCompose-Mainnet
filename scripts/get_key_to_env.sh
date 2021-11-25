b056662ba6007edff7616fb9c835fac89103053d
Kiseop Lee <kiseop.lee@gmail.com>
2021-11-25 15:24:14 +0900

    .env path changed
    .env path changed

--- scripts/get_key_to_env.sh
+++ scripts/get_key_to_env.sh
@@ -3,5 +3,5 @@
 PRIV_KEY=$(sudo head -n 1 /var/lib/docker/volumes/server_validator-key-volume/_data/validator.priv)
 PUB_KEY=$(sudo head -n 1 /var/lib/docker/volumes/server_validator-key-volume/_data/validator.pub)
 
-sed -i "s/private_key/$PRIV_KEY/g" $CREDITCOIN_HOME_UNDER/.env
-sed -i "s/public_key/$PUB_KEY/g" $CREDITCOIN_HOME_UNDER/.env
+sed -i "s/private_key/$PRIV_KEY/g" ~/.env
+sed -i "s/public_key/$PUB_KEY/g" ~/.env
